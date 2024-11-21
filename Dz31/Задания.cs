d39c0e28e5fac90d39afdc5bc54fb51e1672bfef
Андрей Мингалёв <ammingalev@gmail.com>
2024-11-21 18:08:58 +0400

    Тумаков + Задания

---
+++ Dz31/Задания.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+
+
+public enum Day
+{
+    Понедельник,
+    Вторник,
+    Среда,
+    Четверг,
+    Пятнциа,
+    Суббота,
+    Воскресенье
+}
+
+public partial class Programm
+{
+    static void Starttask(byte n)
+    {
+        Console.WriteLine($"Задание: {n}");
+    }
+
+    static void Task1()
+    {
+        Starttask(1);
+
+        // Массив для чисел
+        int[] nums = new int[10];
+
+        Console.WriteLine($"Введите число от {int.MinValue} до {int.MaxValue}\r\nПосле ввод нажмите 'Enter' и введите новое число");
+
+        for (int i11 = 0; i11 < nums.Length; i11++)
+        {
+            nums[i11] = int.Parse(Console.ReadLine());
+        }
+
+        // Введём значение, для начала сравнения
+        int znach = nums[0];
+
+        for (int i12 = 0; i12 < nums.Length; i12++)
+        {
+            if (i12 == 0)
+            {
+                continue;
+            }
+            else if (nums[i12] > znach)
+            {
+                znach = nums[i12];
+                continue;
+            }
+            else
+            {
+                Console.WriteLine($"Число под номером {i12 + 1} нарушает возрастающую последовательнось!");
+                break;
+            }
+        }
+    }
+
+    static void Task2()
+    {
+        Starttask(2);
+
+        string[] cards = new string[]
+        {
+            "Шесёрка",
+            "Семёрка",
+            "Восьмёрка",
+            "Девятка",
+            "Десятка",
+            "Валет",
+            "Дама",
+            "Король",
+            "Туз"
+        };
+
+        try
+        {
+            Console.WriteLine("Введите пожалуйста число 6 - 14");
+            byte card = byte.Parse(Console.ReadLine());
+
+            if (card > 5 && card < 15)
+            {
+                card -= 6;
+                Console.WriteLine($"Ваша карта: {cards[card]}");
+            }
+            else { Console.WriteLine("Вы ввели число, не принадлежащее промежутку 6 - 14"); }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Вы ввели не число! Пожалуйста, в следующий раз введите число 6 - 14");
+        }
+    }
+
+    static void Task3()
+    {
+        Starttask(3);
+
+        // Создадим два массива для напитков и посетителей
+        string[] alcoinput = new string[]
+        {
+            "Jabroni",
+            "SCounselor",
+            "Programmer",
+            "BGM",
+            "Politician",
+            "Rapper"
+        };
+        string[] alcooutput = new string[]
+        {
+            "Patron Tequila",
+            "Anything with Alcohol",
+            "Hipster Craft Beer",
+            "Moonshine",
+            "Your tax dollars",
+            "Cristal",
+            "Beer"
+        };
+
+        Console.WriteLine("Введите социальный статус посетителя и узнайте его самый любимый напиток!");
+        string drink = Console.ReadLine();
+
+        for (int i3 = 0; i3 < alcoinput.Length; i3++)
+        {
+            if (alcoinput[i3].Contains(drink, StringComparison.OrdinalIgnoreCase))
+            {
+                Console.WriteLine($"ДА, он у нас! Он любит пить {alcooutput[i3]}.");
+                break;
+            }
+            else if (i3 == alcoinput.Length - 1)
+            {
+                Console.WriteLine("Простите, но у нас нет такого покупателя...");
+            }
+        }
+    }
+
+    static void Task4()
+    {
+        Starttask(4);
+
+        try
+        {
+            Console.WriteLine("Ввидете день недели:");
+            byte day = byte.Parse(Console.ReadLine());
+
+            if (day > 0 && day < 8)
+            {
+                // Введём переменную, которая будет забирать из перечисления названия дня по его индексу
+                Day dayofweek = (Day)day - 1;
+
+
+                Console.WriteLine($"Сегодня {dayofweek}!");
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели число, которое не является днём недели!");
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Вы ввели не число!");
+        }
+        finally
+        {
+            Console.WriteLine("Спасибо за попытку!");
+        }
+    }
+
+    static void Task5()
+    {
+        Starttask(5);
+
+        Console.WriteLine("Введите кол-во кукол, которые вы хотели, чтобы мы рассмотрели на добавление в нашу книгу!");
+        byte quan = byte.Parse(Console.ReadLine());
+
+        // Создадим список для добавления в него кукол
+        List<string> dolls = new List<string>();
+
+        Console.WriteLine("Введите пожалуйста названия кукол!");
+
+        for (int i51 = 0; i51 < quan; i51++)
+        {
+            string doll = Console.ReadLine();
+            dolls.Add(doll);
+        }
+
+        byte countdoll = 0;
+
+        foreach (string i52 in dolls)
+        {
+            if ((i52.Contains("Hello Kitty", StringComparison.OrdinalIgnoreCase)) || (i52.Contains("Barbie doll", StringComparison.OrdinalIgnoreCase)))
+            {
+                countdoll++;
+            }
+        }
+
+        Console.WriteLine($"Мы добавили в книгу {countdoll} ваших кукол!");
+    }
+    public static void Main()
+    {
+        Task1();
+        Task2();
+        Task3();
+        Task4();
+        Task5();
+    }
+}
---
+++ Тумаков/Тумаков.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+
+public partial class Program
+{
+    static void StartTask(byte n)
+    {
+        Console.WriteLine($"\n\nЗадание {n}");
+    }
+    static void Task1(ushort year, ushort daythisyear)
+    {
+        StartTask(1);
+
+        // Ввод года и дня в этом году
+        Console.Write("Введите год: ");
+        year = ushort.Parse(Console.ReadLine());
+
+        Console.Write("Введите день в этом году: ");
+        daythisyear = ushort.Parse(Console.ReadLine());
+
+        // Получение даты и времени
+        // Добавляем дни, которые ввел пользователь, предварительно отняв один, т.к. считается,
+        // что уже 1 Января!
+        DateTime date = new DateTime(year, 1, 1).AddDays(daythisyear - 1);
+
+        // Вывод полученной даты на консоль
+        Console.WriteLine("Дата: {0:dd-MM-yyyy}", date);
+    }
+
+    static void Task2(string year, string daythisyear)
+    {
+        StartTask(2);
+
+        ushort copyYear = 0;
+        ushort copyDay = 0;
+
+        // Ввод года и дня в этом году
+        Console.Write("Введите год: ");
+        year = Console.ReadLine();
+
+        Console.Write("Введите день в этом году: ");
+        daythisyear = Console.ReadLine();
+
+        try
+        {
+            copyYear = ushort.Parse(year);
+            copyDay = ushort.Parse(daythisyear);
+
+            if (copyDay >= 1 && copyDay <= 365)
+            {
+                DateTime date = new DateTime(copyYear, 1, 1).AddDays(copyDay - 1);
+                Console.WriteLine("Дата: {0:dd-MM-yyyy}", date);
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не число!!!\r\nИли число, которое не подходит!!!");
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Вы ввели не число!!!\r\nИли число, которое не подходит!!!");
+        }
+    }
+    static void Task3(string year, string daythisyear)
+    {
+        StartTask(3);
+
+        ushort copyYear = 0;
+        ushort copyDay = 0;
+
+        // Ввод года и дня в этом году
+        Console.Write("Введите год: ");
+        year = Console.ReadLine();
+
+        Console.Write("Введите день в этом году: ");
+        daythisyear = Console.ReadLine();
+
+        try
+        {
+            copyYear = ushort.Parse(year);
+            copyDay = ushort.Parse(daythisyear);
+
+            //Проверка на ВИСОКОСНЫЙ ГОД, хотя DateTime умеет работать с високосными годами
+            bool flag = (copyYear % 400 == 0 || (copyYear % 4 == 0 && copyYear % 100 != 0));
+
+
+            if (flag)
+            {
+                Console.WriteLine("Это ВИСОКОСНЫЙ год!");
+            }
+            else
+            {
+                Console.WriteLine("Это НЕ ВИСОКОСНЫЙ год!");
+            }
+
+            if (copyDay >= 1 && copyDay <= 365)
+            {
+                DateTime date = new DateTime(copyYear, 1, 1).AddDays(copyDay - 1);
+                Console.WriteLine("Дата: {0:dd-MM-yyyy}", date);
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели не число!!!\r\nИли число, которое не подходит!!!");
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Вы ввели не число!!!\r\nИли число, которое не подходит!!!");
+        }
+    }
+    static void Main()
+    {
+        string year23 = "";
+        string daythisyear23 = "";
+        ushort year1 = 0;
+        ushort daythisyear1 = 0;
+
+        Task1(year1, daythisyear1);
+        Task2(year23, daythisyear23);
+        Task3(year23, daythisyear23);
+    }
+}
